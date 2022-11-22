0852a65d549f4c29c88a9bf40ef693747451fb0d
Андрей Ларин <andrewlarin17@gmail.com>
2022-11-22 22:36:00 +0300

    Добавьте файлы проекта.

---
+++ Task_1/ISeries.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    interface ISeries
+    {
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue, curentValue;
+        public void setStep(int s)
+        {
+            step = s;
+        }
+
+        public int getNext()
+        {
+            curentValue += step;
+            return curentValue;
+        }
+
+        public void reset()
+        {
+            curentValue = startValue;
+            Console.WriteLine("Reset");
+
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            curentValue = startValue;
+        }
+
+    }
+    class GeomProgression : ISeries
+    {
+        int step;
+        int startValue, curentValue;
+        public void setStep(int s) 
+        {
+            step = s;
+        }
+
+        public int getNext()
+        {
+            curentValue *= step;
+            return curentValue;
+        }
+
+        public void reset()
+        {
+            curentValue = startValue;
+            Console.WriteLine("Reset");
+        }
+
+        public void setStart(int x)
+        {
+            startValue = x;
+            curentValue = startValue;
+        }
+    }
+
+}
---
+++ Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            Console.WriteLine("Введите первый элемент арифметической прогрессии");
+            arith.setStart(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите шаг прогрессии");
+            arith.setStep(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+
+            GeomProgression geom = new GeomProgression();
+            Console.WriteLine("Введите первый элемент геометрической прогрессии");
+            geom.setStart(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Введите знаменатель прогрессии");
+            geom.setStep(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();    
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
